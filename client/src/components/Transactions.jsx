8505f7d12e6265d4a8e40a497928101547eb8770
roshan0926 <80285716+roshan0926@users.noreply.github.com>
2022-02-10 00:44:43 +0000

    add logic for sending ethereum between accounts

--- client/src/components/Transactions.jsx
+++ client/src/components/Transactions.jsx
@@ -1,9 +1,40 @@
-import React from 'react';
+import React, { useContext } from 'react';
+import { TransactionContext } from '../context/TransactionContext';
+import dummyData from '../utils/dummyData';
+import { shortenAddress } from '../utils/shortenAddress'
+
+const TransactionCard = ({ addressTo, addressFroom, timestamp, message, keyword, amount, url }) => {
+  return (
+    <div className='bg-[#181918] m-4 flex flex-1
+    2xl:min-w-[450px]
+    2xl:max-w-[500px]
+    sm:min-w-[270px]
+    sm:max-w-[300px]
+    flex-col p-3 rounded-md hover:shawdow-2xl
+    '>
+      <div className='flex flex-col items-center w-full mt-3'>
+
+      </div>
+    </div>
+  )
+}
 
 const Transactions = () => {
-  return <div>
-    <h1>Transactions</h1>
-  </div>;
+  const { currentAccount } = useContext(TransactionContext)
+  return (
+    <div className='flex w-full justify-center items-center 2xl:px-20 gradient-bg-transactions'>
+      <div className='flex flex-col md:p-12 py-12 px-4'>
+        {currentAccount ?
+          (<h3 className='text-white text-3xl text-center my-2'>Latest Transactions</h3>) :
+          (<h3 className='text-white text-3xl text-center my-2'>Connect Your Account to See the Latest Transactioins</h3>)}
+        <div className='flex flex-wrap justify-center items-center mt-10'>
+          {dummyData.reverse().map((transaction, i) => (
+            <TransactionCard key={i} {...transaction} />
+          ))}
+        </div>
+      </div>
+    </div>
+  )
 };
 
 export default Transactions;
