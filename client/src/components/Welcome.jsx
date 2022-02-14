baed2a962036ad2b8fce7bc2e0f827a6ecd24aa5
roshan0926 <80285716+roshan0926@users.noreply.github.com>
2022-02-14 00:07:34 +0000

    fix field name error

--- client/src/components/Welcome.jsx
+++ client/src/components/Welcome.jsx
@@ -19,7 +19,7 @@ const Input = ({ placeholder, name, type, value, handleChange }) => (
   />
 )
 const Welcome = () => {
-  const { connectWallet, currentAccount, formData, handleChange, sendTransaction } = useContext(TransactionContext);
+  const { connectWallet, currentAccount, formData, handleChange, sendTransaction, isLoading } = useContext(TransactionContext);
 
   const handleSubmit = (e) => {
     const { addressTo, amount, keyword, message } = formData;
@@ -76,10 +76,10 @@ const Welcome = () => {
           <div className='p-5 sm:w-96 w-full flex flex-col justify-start item-center blue-glassmorphism'>
             <Input placeholder='Address To' name='addressTo' type='text' handleChange={handleChange} />
             <Input placeholder='Amount (ETH)' name='amount' type='number' handleChange={handleChange} />
-            <Input placeholder='Keyword (Auto Generated Gif)' name='keyword' type='text' handleChange={handleChange} />
-            <Input placeholder='Enter Message' name='message' type='text' handleChange={handleChange} />
+            <Input placeholder='Enter Message' name='keyword' type='text' handleChange={handleChange} />
+            <Input placeholder='Keyword (Auto Generated Gif)' name='message' type='text' handleChange={handleChange} />
             <div className='h-[1px] w-full bg-gray-400 my-2' />
-            {false ? (<Loader />) : (<button type='button' onClick={handleSubmit} className='text-white w-full mt-2 border-[1px] p-2 border-[#3d4f7c] rounded-full curson-pointer'>
+            {isLoading ? (<Loader />) : (<button type='button' onClick={handleSubmit} className='text-white w-full mt-2 border-[1px] p-2 border-[#3d4f7c] rounded-full curson-pointer'>
               Send Now
             </button>)}
           </div>
